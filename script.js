178104cd2d4eeaa01b4aa0e2d6ff4c386f8a07f6
jenDoeMoldovan <michalevic@gmail.com>
2021-10-28 10:27:25 +0300

    created a Create SVG button which creates a new SVG elemnt according to the drawing the user has created on the drawingBoard

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ const currentColor = document.querySelector(".colorPicker");
 const eraser = document.querySelector(".erase");
 const reset = document.querySelector(".reset");
 const draw = document.querySelector(".paint");
+const create = document.querySelector(".createSVG");
 let paintOrErase = true;
 
 function createBoard(drawingBoard) {
@@ -43,4 +44,20 @@ draw.onclick = function (e) {
   draw.classList.add("buttonPressed");
   eraser.classList.remove("buttonPressed");
   paintOrErase = true;
+};
+
+create.onclick = function (e) {
+  const newSVG = document.createElement("svg");
+  newSVG.classList.add("newSVG");
+  square.forEach((e) => {
+    const newSquare = document.createElement("rect");
+    newSquare.width = "20";
+    newSquare.width = "20";
+    newSquare.style.backgroundColor = e.style.backgroundColor;
+    newSVG.appendChild(newSquare);
+    console.log(newSquare);
+  });
+  console.log(newSVG);
+
+  document.body.appendChild(newSVG);
 };
